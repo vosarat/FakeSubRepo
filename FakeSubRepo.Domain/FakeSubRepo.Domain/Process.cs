123dbac03dcfb4377b8aefd81124886c9239fc53
tarasov_es <vosarat1995@gmail.com>
2018-12-19 15:28:24 +0300

    Добавлен процесс

---
+++ FakeSubRepo.Domain/FakeSubRepo.Domain/Process.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FakeMainRepo.Domain;
+
+namespace FakeSubRepo.Domain
+{
+	class Process
+	{
+		public Worker worker;
+
+		public Process(Worker worker)
+		{
+			this.worker = worker;
+		}
+	}
+}
